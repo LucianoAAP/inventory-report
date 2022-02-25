5557628963dba359a34095b0d893d38ee0cfe76e
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-02-25 18:12:45 -0300

    Group 41 Retira funções (#164)
    * REFACTOR: Retira a função desnecessária
    
    Co-authored-by: Luciano AAP <lucianoalmeidaap@gmail.com>

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,13 +1,4 @@
 from abc import ABC, abstractmethod
-from inventory_report.reports.complete_report import CompleteReport
-from inventory_report.reports.simple_report import SimpleReport
-
-
-def generate_report(report, type):
-    if type == 'completo':
-        return CompleteReport.generate(report)
-    else:
-        return SimpleReport.generate(report)
 
 
 class Importer(ABC):
